92a25b5af8c2c764bfe5bfc634c544bec272e4fc
David3232 <davidfdezcas@gmail.com>
2019-01-17 09:57:53 +0100

    Solution first problems with index

--- floridaNews/src/app/app.module.ts
+++ floridaNews/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangeTopicComponent } from './change-topic/change-topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangeTopicComponent
   ],
   imports: [
     BrowserModule,
---
+++ floridaNews/src/app/change-topic/change-topic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-topic',
+  templateUrl: './change-topic.component.html',
+  styleUrls: ['./change-topic.component.css']
+})
+export class ChangeTopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
