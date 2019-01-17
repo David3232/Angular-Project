2c94aa271a482faab93e6fa14619e4c862e52b24
David3232 <davidfdezcas@gmail.com>
2019-01-17 11:50:20 +0100

    Change topic halfway through work

--- floridaNews/src/app/change-topic/change-topic.component.ts
+++ floridaNews/src/app/change-topic/change-topic.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-topic.component.css']
 })
 export class ChangeTopicComponent implements OnInit {
+  title: string = 'Podria interesarte: ';
+  topic: string = 'economy';
+  imgSports: string = "../../assets/img/Julio.jpg";
+  imgEconomy: string = "../../assets/img/Robin.jpg";
+  imgCulture: string = "../../assets/img/julen.jpg";
 
-  constructor() { }
+  changeTopic() {
+    if (this.topic === 'economy') {
+      this.topic = 'sports';
+    }  else if (this.topic === 'sports') {
+      this.topic = 'culture';
+    } else if (this.topic === 'culture')  {
+      this.topic = 'economy';
+    }
+  }
 
   ngOnInit() {
+
   }
+
 
 }
